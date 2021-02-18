e3847bfef6ae76279d89a4664848a98406761a8b
Karen Gabriella Rodrigues dos Santos <79243574+karengabriella@users.noreply.github.com>
2021-02-18 17:07:21 -0300

    Update exercicio10.java

--- src/EDexercicios/exercicio10.java
+++ src/EDexercicios/exercicio10.java
@@ -1,35 +1,29 @@
-//Exercicio 10 - Receba um número inteiro. 
-//Calcule e mostre a série de Fibonacci recursivamente até o
-//número recebido
+//Exercicio 10 - Receba um nÃºmero inteiro. 
+//Calcule e mostre a sÃ©rie de Fibonacci recursivamente atÃ© o
+//nÃºmero recebido
 
 package EDexercicios;
 import javax.swing.JOptionPane;
 
 public class exercicio10 {
-
-    public static void main(String[] args) {
-    	
-    	int termo;
-        termo = Integer.parseInt(JOptionPane.showInputDialog(("Termo Fibonacci: ")));
-        System.out.print("Sequência: ");
-        fibonacci(termo,0,0,1);
-    			
-     }  
+  public static void main(String[] args) {
+     int termo;
+     termo = Integer.parseInt(JOptionPane.showInputDialog(("Termo Fibonacci: ")));
+     System.out.print("SequÃªncia: ");
+     fibonacci(termo,0,0,1); 	//FunÃ§Ã£o Recursiva 		
+  }  
     
-//Método Recursivo
-    
-     public static int fibonacci(int num, int aux, int var1, int var2){
+//----------------
+  public static int fibonacci(int num, int aux, int var1, int var2){
   	  
  	   if(num == 0)
- 		   return aux;  
+ 	     return aux;  
  	   else
-         
  	    aux = var1 + var2;
  	    var1 = var2;
  	    var2 = aux;
  	    System.out.print(" " + aux);
  	    
  	   return fibonacci(num-1, aux, var1, var2);  
-    }			
-	
+          }				
   }
