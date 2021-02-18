9ef444d1163fa53994fbc81a52058db38fd85f7d
Karen Gabriella Rodrigues dos Santos <79243574+karengabriella@users.noreply.github.com>
2021-02-18 16:15:17 -0300

    Update exercicio01.java

--- src/EDexercicios/exercicio01.java
+++ src/EDexercicios/exercicio01.java
@@ -1,7 +1,7 @@
-//-----Exercício 01 - Carregar um vetor [100] real de valores de saldos. 
+//-----ExercÃ­cio 01 - Carregar um vetor [100] real de valores de saldos. 
 // - Calcular e exibir:
-// - Média dos saldos positivos entre 100 à 1000
-// - Média geral dos saldos
+// - MÃ©dia dos saldos positivos entre 100 Ã  1000
+// - MÃ©dia geral dos saldos
 // - Soma dos saldos negativos
 
 package EDexercicios;
@@ -9,76 +9,75 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
  public class exercicio01 {
-	public static void main(String[] args) {
+ public static void main(String[] args) {
 	    
     	Random aleatorios = new Random();
-		int valores[] = new int [100];  	
-        int i;
-		for(i = 0; i<20; i++){
-			valores[i] = aleatorios.nextInt(2500)-1500; //Valores aleatórios de -1500 a 2500
-	      }
-		
-//---Opções - chamada de funções resolutivas.
-		   int opc = 0;
-		   while (opc != 4) {
-			  opc = Integer.parseInt(JOptionPane.showInputDialog((" \n 1 - Media de Saldos Positivos \n 2 - Media Geral dos Saldos \n 3 - Media de Saldos Negativos \n 4 - Encerrar")));
-		   switch (opc) {
-		   	case 1:
-		      Media(valores);
-		      break;
-		   	case 2:
-		   	    MediaGeral(valores);
-		   	    break;
-		   	case 3:
-		   		MediaNegativos(valores);
-		   	case 4:
-		   		break;
-			 }
-		   }
+	 int valores[] = new int [100];  	
+         int i;
+	 for(i = 0; i<20; i++){
+       	   valores[i] = aleatorios.nextInt(2500)-1500; //Valores aleatÃ³rios de -1500 a 2500
 	  }
+		
+//---OpÃ§Ãµes - chamada de funÃ§Ãµes resolutivas.
+        int opc = 0;
+        while (opc != 4) {
+          opc = Integer.parseInt(JOptionPane.showInputDialog((" \n 1 - Media de Saldos Positivos \n 2 - Media Geral dos Saldos \n 3 - Media de Saldos Negativos \n 4 - Encerrar")));
+         switch (opc) {
+   	   case 1:
+	     Media(valores);  //MÃ©dia de Saldos positivos entre 100 a 1000
+	     break;
+	   case 2:
+	     MediaGeral(valores);  //MÃ©dia geral de todos os saldos
+	     break;
+           case 3:
+	     MediaNegativos(valores);  //
+	     break;
+           case 4:
+	      break;
+	    }
+	 }
+  }
 
- //--- Média dos Positivos de 100 a 1000
-			
-       public static void Media(int valorestotais[]){	
-		 	int i, aux = 0;
-			double media = 0;
-			for (i = 0; i< valorestotais.length; i++) {    
-			   if(valorestotais[i] >= 10 && valorestotais[i] <= 100){
-			        media += valorestotais[i];
-			         aux ++;	   
-			    }
-					
-			  }
-				 media = media/aux;
-			     System.out.println("Média dos Saldos positivos de 100 até 1000:  " + media);
-			  }
+ //--- MÃ©dia dos Positivos de 100 a 1000
+
+ public static void Media(int valorestotais[]){	
+        int i, aux = 0;
+        double media = 0;
+       for (i = 0; i< valorestotais.length; i++) {    
+           if(valorestotais[i] >= 10 && valorestotais[i] <= 100){
+		media += valorestotais[i];
+	        aux ++;	   
+	    }					
+         }
+	 media = media/aux;
+	 System.out.println("MÃ©dia dos Saldos positivos de 100 atÃ© 1000:  " + media);
+  }
 			
  //----Media Geral dos Saldos
 			
-	    public static void MediaGeral(int valorestotais[]){
-			 int i;
-			 double media = 0;
-			 for (i = 0; i< valorestotais.length; i++) {   
-			       media += valorestotais[i];   
-			  }		
-			  	media = media/valorestotais.length;
-			    System.out.println("Média Geral dos Saldos:  " + media);
-			  }
+ public static void MediaGeral(int valorestotais[]){
+         int i;
+	 double media = 0;
+	for (i = 0; i< valorestotais.length; i++) {   
+             media += valorestotais[i];   
+         }		
+	media = media/valorestotais.length;
+	System.out.println("MÃ©dia Geral dos Saldos:  " + media);
+   }
 			
  //----Media dos Saldos Negativos
 		 	
-		 public static void MediaNegativos(int valorestotais[]){
-			  int i, aux = 0;
-		 	  double media = 0;
-		 	   for (i = 0; i< valorestotais.length; i++) {    
-		 		 if(valorestotais[i] < 0){
-		 	         media += valorestotais[i];
-		 	          aux ++;	   
-		 		  }
-		 			
-		 		}
-		 			media = media/aux;
-		 	        System.out.println("Média dos Saldos negativos:  " + media);
-		 	  }
+ public static void MediaNegativos(int valorestotais[]){
+         int i, aux = 0;
+	 double media = 0;
+         for (i = 0; i< valorestotais.length; i++) {    
+	     if(valorestotais[i] < 0){
+		 media += valorestotais[i];
+		 aux ++;	   
+	     }		 			
+	 }
+	 media = media/aux;
+	 System.out.println("MÃ©dia dos Saldos negativos:  " + media);
+  }
       	
  }
