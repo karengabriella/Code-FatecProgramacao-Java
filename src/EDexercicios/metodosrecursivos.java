db68d5738f4c8d82eb1330a237b1eba6e4d47cf7
Karen Gabriella Rodrigues dos Santos <79243574+karengabriella@users.noreply.github.com>
2021-02-18 17:01:42 -0300

    Update metodosrecursivos.java

--- src/EDexercicios/metodosrecursivos.java
+++ src/EDexercicios/metodosrecursivos.java
@@ -6,61 +6,50 @@ public class metodosrecursivos {
       
       public int fatorial(int num){
     	  
-      	if(num == 0){
-  			return 1;
-  		}
-  		return num * fatorial(num-1);
-  	 }
-  	  
-  
-   
-      
+       	  if(num == 0){
+  	    return 1;
+  	   }
+  	   return num * fatorial(num-1);
+       }
+  	        
 //Exercicio 07 - Soma Total
    
    public int somatotal(int num){
-	     
+    
 	   if(num == 0){
-		   return num;
+	     return num;
 	   }
-       
 	   return num += somatotal(num-1);
-      
-     }  
-   
-   
+    }  
+    
 //Exercicio 08 - Soma dos Pares
    
    public int somapares(int num, int aux){  
 	   
 	   if(num % 2 == 0 || num == 2){
-		   aux += num;
+	      aux += num;
 	   }
-	   
 	   if(num == 0){
-		   return aux;
+	      return aux;
 	   }
-       
-	   return num = somapares(num-1,aux);
-      
-     }
+	   return num = somapares(num-1,aux); 
+    }
 
-//Exercicio 09 - Soma dos ímpares
+//Exercicio 09 - Soma dos Ã­mpares
    
    public int somaimpares(int num, int aux){
 	   
 	   if(num % 2 != 0){
-		   aux += num;
+	      aux += num;
 	   }
 	   
 	   if(num == 0){
-		   return aux;
-       }
-       
-	   return num = somaimpares(num-1,aux);
-      
-     }
+	     return aux;
+           }
+	   return num = somaimpares(num-1,aux);   
+    }
    
-  }//Fim
+ }
 
 		 
 		
