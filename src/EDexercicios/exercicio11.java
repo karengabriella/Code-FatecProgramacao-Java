02ec0a96046bd51c7f87d6c918147ea973de5066
Karen Gabriella Rodrigues dos Santos <79243574+karengabriella@users.noreply.github.com>
2021-02-18 17:08:56 -0300

    Update exercicio11.java

--- src/EDexercicios/exercicio11.java
+++ src/EDexercicios/exercicio11.java
@@ -1,30 +1,29 @@
-//Exercicio 11 - Receba um número. Calcule e mostre a série 
+//Exercicio 11 - Receba um nÃºmero. Calcule e mostre a sÃ©rie 
 //1 + 1/2 + 1/3 + ... + 1/N recursivamente. 
 
 package EDexercicios;
 import javax.swing.JOptionPane;
 
 public class exercicio11 {
+ public static void main(String[] args) {
+        int num;
+    	num = Integer.parseInt(JOptionPane.showInputDialog("Termo final da SÃ©rie: "));
+	System.out.print("SÃ©rie: \n 1 ");
+	for (int i = 0; i < num; i++) {
+	   System.out.print(" + 1/" + (i+1));
+	}		
+        System.out.println("\n Soma: " + serie(num));
+}
 
-	public static void main(String[] args) {
-	    int num;
-    	num = Integer.parseInt(JOptionPane.showInputDialog("Termo final da Série: "));
-		System.out.print("Série: \n 1 ");
-		for (int i = 0; i < num; i++) {
-		  System.out.print(" + 1/" + (i+1));
-		}		
-		  System.out.println("\n Soma: " + serie(num));
-	  	}
-
-//---Método Recursivo Calculo da Série 
+//-----Calculo da SÃ©rie 
 	
-	public static double serie(int num) {
-	   if (num == 1)
-		 return 1.0;
-	  else
-        	return 1.0 / num + serie(num - 1);
-			}
-    }
+ public static double serie(int num) {
+	 if (num == 1)
+	   return 1.0;
+	 else
+          return 1.0 / num + serie(num - 1);
+  }
+}
 		
 	
 
